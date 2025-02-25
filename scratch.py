c54faa9da4e2b98c8dfe3a23230bdad3c0d2198d
Isaiah <32691164+IMMachinations@users.noreply.github.com>
2025-02-25 17:52:00 -0600

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -1,3 +1,29 @@
+#%%
+import torch as t
+import transformer_lens
+from transformers import AutoModelForCausalLM
 
+
+device = t.device("mps" if t.backends.mps.is_available() else "cuda" if t.cuda.is_available() else "cpu")
+print(device)
+# %%
+r1QwenDistillSmall = AutoModelForCausalLM.from_pretrained("deepseek-ai/DeepSeek-R1-Distill-Qwen-7B")
+print(dir(r1QwenDistillSmall))
+
+# %%
+
+model = transformer_lens.HookedTransformer.from_pretrained_no_processing(
+    "Qwen/Qwen2.5-7B",
+    r1QwenDistillSmall,
+    device=device
+)
+# %%
+
+model = transformer_lens.HookedTransformer.from_pretrained("Qwen/Qwen2.5-1.5B-Instruct")
+print(model.parameters)
+# %%
+output = model.generate("There are three cities: The city of Gold, The city of Iron, and The City of Rubber. \nThree wise men" +
+                        " approach the cities: One young, one tall, and one bold. \nWhich man goes to which city?<think>\n", max_new_tokens=1000, temperature =0.6)
+print(output)
 
 
